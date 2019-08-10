7e3351c462b7f9091ff50c36c5911b8d4e79215c
Chamath Jeevan <chamathjeevan@gmail.com>
2019-08-11 03:41:44 +0530

    HS Code

--- routers/hs.js
+++ routers/hs.js
@@ -5,7 +5,16 @@ const Joi = require('@hapi/joi');
 
 router.get('/:Client_ID/hs/', function(req, res, next) {
 
-    dbConnection.query('SELECT * FROM BRIDGE.Hs' , function(error, results, fields){
+    dbConnection.query('SELECT * FROM Hs WHERE Client_ID = ?',req.params.Client_ID , function(error, results, fields){
+        if (error) return next (error);
+        if (!results|| results.length==0) return res.status(404).send();
+        return res.send(results)
+    })
+})
+
+router.get('/:Client_ID/hs/:id', function(req, res, next) {
+
+    dbConnection.query('SELECT * FROM Hs WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.id], function(error, results, fields){
         if (error) return next (error);
         if (!results|| results.length==0) return res.status(404).send();
         return res.send(results)
@@ -35,10 +44,9 @@ router.put ('/:Client_ID/hs/',function(req, res, next){
     })
 })
 
-router.post('/Client_ID/hs/', function(req,res){
-    const schema = Joi.object().keys({
-        Client_ID: Joi.string().alphanum().min(3).max(30).required(),
-        Description:  Joi.string().alphanum().min(3).max(30).required()
+router.post('/:Client_ID/hs/', function(req,res){
+   const schema = Joi.object().keys({
+    HsCode: Joi.string().alphanum().min(3).max(30).required()
     })
 
     Joi.validate(req.body, schema, (err, result)=> {
@@ -48,86 +56,56 @@ router.post('/Client_ID/hs/', function(req,res){
         }
          
         let hs = req.body;
-        dbConnection.query("INSERT INTO BRIDGE.Hs SET ID = Client_ID = ?". req.body, function(error, results, fields){
-            if (error) return next(error);
-            return res.status(404).send();
-        })
+        const uuidv4 = require('uuid/v4')
+        let userID = req.header('InitiatedBy')
+        let clientID = req.header('Client_ID')
+
+
+        let hsCode = {
+            HsCode: HS_ID,
+            Client_ID:clientID,
+            Description: hs.Description,
+            Unit: hs.Unit,
+            GenDuty: hs.GenDuty,
+            VAT: hs.VAT,
+            PAL: hs.PAL,
+            NTB: hs.NTB,
+            Cess: hs.Cess,
+            Excise: hs.Excise,
+            SCL: hs.SCL,
+            MType:hs.MType
+        }
+        dbConnection.query("INSERT INTO HS SET ? ", hsCode, function(error1, results, fields) {
+            if (error) {
+                console.error(error1);
+                res.status(500).send(error1);
+            } else {
+                if (!results || results.length == 0) {
+                    res.status(404).send();
+                } else {
+                    res.status(201).send({
+                        error: false,
+                        data: results,
+                        message: 'New regulatory approval has been created successfully.'
+                    });
+                }
+            }
+            res.end();
+            return
+        });
     })
 })
 
 
-
 const schema = Joi.object().keys({
-    HsCode: Joi.string().alphanum().min(3).max(30).required(),
-    ClientName: Joi.string().trim().required(),
-    Priority: Joi.string().trim().required()
+    HsCode: Joi.string().alphanum().min(3).max(30).required()
+   
 })
 
-router.get('/hs', function (req, res, next) {
 
-    dbConnection.query('SELECT * FROM BRIDGE.Hs', function (error, results, fields) {
+router.delete('/:Client_ID/hs//:id', function (req, res, next) {
 
-        if (error) return next(error);
-
-        if (!results || results.length == 0) return res.status(404).send()
-
-        return res.send(results)
-    });
-
-});
-
-router.get('/hs/:id', function (req, res, next) {
-
-    let Hs_ID = req.params.id;
-
-    dbConnection.query('SELECT * FROM BRIDGE.Hs where HsCode = ?', Hs_ID, function (error, results, fields) {
-
-        if (error) return next(error);
-
-        if (!results || results.length == 0) return res.status(404).send()
-
-        return res.send(results);
-
-    });
-});
-
-router.post('/hs', function (req, res, next) {
-
-    Joi.validate(req.body, schema, (err, result) => {
-        if (err) {
-            return res.status(400).send();
-        }
-
-        dbConnection.query("INSERT INTO BRIDGE.Hs SET ? ", req.body, function (error, results, fields) {
-
-            if (error) return next(error);
-
-            return res.status(201).send();
-        });
-    });
-});
-
-router.put('/hs', function (req, res) {
-
-    Joi.validate(req.body, schema, (err, result) => {
-        if (err) {
-            return res.status(400).send();
-        }
-        let hs = req.body;
-        dbConnection.query("UPDATE BRIDGE.Hs SET ClientName = ?, Priority = ? WHERE HsCode = ? ", [hs.ClientName, hs.Priority, hs.HsCode], function (error, results, fields) {
-
-            if (error) return next(error);
-
-            if (!results || results.affectedRows == 0) res.status(404).send();
-
-            return res.send(results);
-        });
-    });
-});
-
-router.delete('/hs/:HsCode', function (req, res, next) {
-
-    dbConnection.query("DELETE FROM BRIDGE.Hs WHERE HsCode =  ? ", req.params.HsCode, function (error, results, fields) {
+    dbConnection.query('DELETE FROM Hs WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.Client_ID], function (error, results, fields) {
 
         if (error) return next(error);
 
