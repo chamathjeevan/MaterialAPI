14479cd21bdeb3dc3698c4f9bf46e55eb2d77f32
Chamath Jeevan <chamathjeevan@gmail.com>
2019-08-11 08:47:23 +0530

    HS Code refactor

--- routers/hs.js
+++ routers/hs.js
@@ -5,7 +5,7 @@ const Joi = require('@hapi/joi');
 
 router.get('/:Client_ID/hs/', function(req, res, next) {
 
-    dbConnection.query('SELECT * FROM Hs WHERE Client_ID = ?',req.params.Client_ID , function(error, results, fields){
+    dbConnection.query('SELECT * FROM HsCodes WHERE Client_ID = ?',req.params.Client_ID , function(error, results, fields){
         if (error) return next (error);
         if (!results|| results.length==0) return res.status(404).send();
         return res.send(results)
@@ -14,7 +14,7 @@ router.get('/:Client_ID/hs/', function(req, res, next) {
 
 router.get('/:Client_ID/hs/:id', function(req, res, next) {
 
-    dbConnection.query('SELECT * FROM Hs WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.id], function(error, results, fields){
+    dbConnection.query('SELECT * FROM HsCodes WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.id], function(error, results, fields){
         if (error) return next (error);
         if (!results|| results.length==0) return res.status(404).send();
         return res.send(results)
@@ -34,7 +34,7 @@ router.put ('/:Client_ID/hs/',function(req, res, next){
         }
 
         let hs = req.body;
-        dbConnection.querry("UPDATE BRIDGE.Hs SET Client_ID = ?", [Hs.Client_ID] , function(error,results, fields) {
+        dbConnection.querry("UPDATE HsCodes SET Client_ID = ?", [Hs.Client_ID] , function(error,results, fields) {
 
             if (error) return next.body;
             if (!results|| results.affetedRows ==0) res.status(404).send();
@@ -46,23 +46,31 @@ router.put ('/:Client_ID/hs/',function(req, res, next){
 
 router.post('/:Client_ID/hs/', function(req,res){
    const schema = Joi.object().keys({
-    HsCode: Joi.string().alphanum().min(3).max(30).required()
+            HsCode: Joi.string(),
+            Description: Joi.string().required(),
+            Unit:  Joi.string().required(),
+            GenDuty: Joi.number(),
+            VAT: Joi.number(),
+            PAL: Joi.number(),
+            NTB: Joi.number(),
+            Cess: Joi.number(),
+            Excise: Joi.number(),
+            SCL: Joi.number(),
+            MType:Joi.number()
     })
 
     Joi.validate(req.body, schema, (err, result)=> {
         if (err){
+            console.error('ERROR ==> '  + err);
             return res.status(400).send();
-
         }
          
         let hs = req.body;
-        const uuidv4 = require('uuid/v4')
         let userID = req.header('InitiatedBy')
         let clientID = req.header('Client_ID')
 
-
-        let hsCode = {
-            HsCode: HS_ID,
+        let hsCodeObj = {
+            HsCode: hs.HsCode,
             Client_ID:clientID,
             Description: hs.Description,
             Unit: hs.Unit,
@@ -75,10 +83,10 @@ router.post('/:Client_ID/hs/', function(req,res){
             SCL: hs.SCL,
             MType:hs.MType
         }
-        dbConnection.query("INSERT INTO HS SET ? ", hsCode, function(error1, results, fields) {
+        dbConnection.query("INSERT INTO HsCodes SET ? ", hsCodeObj, function(error, results, fields) {
             if (error) {
-                console.error(error1);
-                res.status(500).send(error1);
+                console.error(error);
+                res.status(500).send(error);
             } else {
                 if (!results || results.length == 0) {
                     res.status(404).send();
@@ -103,9 +111,9 @@ const schema = Joi.object().keys({
 })
 
 
-router.delete('/:Client_ID/hs//:id', function (req, res, next) {
+router.delete('/:Client_ID/hs/:id', function (req, res, next) {
 
-    dbConnection.query('DELETE FROM Hs WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.Client_ID], function (error, results, fields) {
+    dbConnection.query('DELETE FROM HsCodes WHERE Client_ID = ? AND HsCode = ? ',[req.params.Client_ID,req.params.Client_ID], function (error, results, fields) {
 
         if (error) return next(error);
 
